c1f89de071c6ae0381a09c189a6054e9425795d3
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-01-27 13:00:00 -0600

    Correcciones
    Se agregaron reestricciones y se modifico el valor a devolver en la calculadora

--- Calculator.java
+++ Calculator.java
@@ -20,6 +20,9 @@ public class Calculator implements ICalculator {
     	//pre: expression es un string el cuál representa una expresión aritmetica en notación postfiz
     	//post: se devuelve el resultado de la operación postfix ingresada, si hay una operación no válida se retornara un valor negativo
     	try {
+    		if (!expression.contains("+") || !expression.contains("-") || !expression.contains("*") || !expression.contains("/")) { //Evaluar si hay operadores en la expresión
+    			throw new ArithmeticException("No hay operadores");
+    		}
     		Stack<Double> stack = new StackVector<Double>(); //esta variable almacena el stack que se usará para realizar las operaciones
     		String[] characters = expression.split("\s"); //esta varaiable divide el string necesario para la operacion y lo divide varios strings. Para hacer la dividsión debe haber un espacio en blanco
     		for (int i=0; i<characters.length; i++) { //Ciclo for el cuál evaluara cada elemento del array de strings
@@ -29,7 +32,7 @@ public class Calculator implements ICalculator {
     			else { //Procedimiento a seguir si el elemento del array no es un número
     				Double firstNum = stack.pop(); //Guardar el último elemento del stack y luego borrarlo
     				Double secondnumb = stack.pop();
-    				switch(characters[i]) { //Evaluar el signo de la operación, se harán operaciones dependiendo el signo
+    				switch(characters[i]) { //Evaluar 6 4 7 + p el signo de la operación, se harán operaciones dependiendo el signo. Guardar el resultado de la operación en el stack
     					
     					case "+":
     						stack.push(firstNum+ secondnumb);
@@ -41,14 +44,15 @@ public class Calculator implements ICalculator {
     						stack.push(firstNum* secondnumb);
     						break;
     					case "/":
-    						stack.push(firstNum- secondnumb);
+    						stack.push(firstNum/ secondnumb);
     						break;
     					
     				}
     			}
     		}
     		this.operationCounter +=1; //Sumar 1 si se utiliza evaluate
-    		return stack.peek(); //Devolver el valor más reciente de la lista
+    		this.lastResult = stack.peek(); //Guardar el valor más reciente en una variable
+    		return stack.pop(); //Devolver el valor más reciente de la lista. Dejar el stack vacío
     	}
     	catch (Exception e){
     		throw new ArithmeticException("Operación incorrecta"); //Indicar que hubo un error
@@ -60,14 +64,14 @@ public class Calculator implements ICalculator {
     public double getPrevious() {
 
     	//post: regresa el valor de la última operación realizada si el valor de operationCounter no es cero, de lo contrario, regresa un valor negativo
-    	double res =0.0;
+    	double res =0.0; //Variable a devolver
 		if (this.operationCounter != 0) {
-			res = this.lastResult;
+			res = this.lastResult; //Guardar el valor de last result
 		}
 		else {
-			res = -1.0;
+			res = -1.0; //Devolver un valor negativo
 		}
-		return res;
+		return res; //Devolver la variable
 		
     }
     
