064b189047e49ee14eeaececa6cd6e9f67eb5641
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-01-27 18:36:21 -0600

    Comentarios
    Se agregaron más comentarios

--- StackOperationsTest.java
+++ StackOperationsTest.java
@@ -2,23 +2,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
+/**
+ * 
+ * @author Jose Anton, Alejandro Ortega, Pedro Pablo Guzmán
+ * @version 1.0.0
+ *
+ */
 class StackOperationsTest {
-	StackVector sv = new StackVector<Integer>();
+	StackVector sv = new StackVector<Integer>(); //Esta variable almacenara el stack que se usará para todas las pruebas
 	@Test
+	/**
+	 * Este método prueba los métodos push y peek
+	 */
 	void PeekandPushMethodtest() {
-		this.sv = new StackVector<Integer>();
-		sv.push(4);
+		this.sv.push(4);
 		int expectedvalue = 4;
-		Assert.assertEquals(expectedvalue, sv.peek());
+		Assert.assertEquals(expectedvalue, this.sv.peek());
 	}
 	
 	@Test
 	void popTest() {
 		this.sv.push(300);
 		int expected = 300;
-		Assert.assertEquals(expected, this.sv.pop());
-		
+		Assert.assertEquals(expected, this.sv.pop());	
 		
 	}
 
