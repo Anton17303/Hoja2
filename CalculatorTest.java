7dd39b6236459c5da3a6e419a18eccda80a593d6
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-01-27 19:26:44 -0600

    JavaDoc
    Se agregaron comentarios javadoc

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,10 +1,18 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
+/**
+ * 
+ * @author Jose Anton, Alejandro Ortega, Pedro Pablo Guzmán
+ * @version 1.0.0
+ *
+ */
 class CalculatorTest {
 
 	@Test
+	/**
+	 * Este método prueba el método evaluate de la clase calculadora
+	 */
 	void Calculatortest() {
 		Calculator calc = new Calculator();
 		Double res = calc.evaluate("6 2 3 + *");
